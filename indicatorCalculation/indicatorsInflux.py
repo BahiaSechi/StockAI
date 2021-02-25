7df86cbf1ad778c7258a49fa00136ebc26dc1974
Clément <clement.servat@ecole.ensicaen.fr>
2021-02-25 13:51:52 +0100

    parsing des données récupérées depuis la bdd

--- indicatorCalculation/indicatorsInflux.py
+++ indicatorCalculation/indicatorsInflux.py
@@ -3,12 +3,24 @@ file : indicatorsInflux.py
 desc : 
 """
 import json
-from influxdb import InfluxDBClient
+from influxdb import DataFrameClient
 import pandas as pd 
+import talib as ta
+import numpy as np
 
-client = InfluxDBClient(host='51.210.180.105', port=8086)
+client = DataFrameClient(host='51.210.180.105', port=8086)
 
-data = client.query('SELECT * FROM "stockai"."autogen"."open" WHERE "name"="IXIC" FILL(null)')
+dfs = client.query('''
+SELECT value 
+FROM "stockai"."autogen"."close" 
+WHERE time > \'2012-12-31\' AND time < \'2019-08-31\' 
+AND ("name"=\'AAPL\' 
+OR "name"=\'MSFT\') 
+GROUP BY "name"
+''')
 
+print(dfs[('close', (('name', 'AAPL'),))].values.flatten('F'))
 
+rsi = ta.RSI(dfs[('close', (('name', 'AAPL'),))].values.flatten('F'), timeperiod=14) #skip 14 days to have real values
 
+print(rsi)
