e73d8b5b5605a6079c11856227e0625ce508f95c
RABOTIN Matéo <mateo@rabotin.net>
2021-02-26 14:47:46 +0100

    Added PriceType to gateway

--- gateway/gatewayInflux.py
+++ gateway/gatewayInflux.py
@@ -7,15 +7,17 @@ from datetime import datetime, timedelta
 from influxdb import DataFrameClient
 from pandas import DataFrame
 
+from gateway.PriceType import PriceType
+
 client = DataFrameClient(host='51.210.180.105', port=8086)
 
 
-def get_data(ticker_name: str, days: int) -> DataFrame:
+def get_data(ticker_name: str, days: int, type: PriceType) -> DataFrame:
     date = datetime.now() - timedelta(days=days)
 
     dfs = client.query(f'''
     SELECT value 
-    FROM "stockai"."autogen"."close"
+    FROM "stockai"."autogen".{type.value}
     WHERE time > \'{date}\' 
     AND ("name"=\'{ticker_name}\')''')
 
