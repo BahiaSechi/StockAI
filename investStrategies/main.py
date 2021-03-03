d13f346de2cb19064602e11638bc4bdf0b103b78
Clément <cmservat@gmail.com>
2021-03-03 09:56:36 +0100

    modification sys.append pour que ce soit universel

--- investStrategies/main.py
+++ investStrategies/main.py
@@ -1,6 +1,6 @@
 import sys
-sys.path.append("/Users/clem/ENSICAEN/2A/stockai/")
 import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__) + '../')
 from investStrategies.MultipleTechInd.Investor import Investor
 
 
