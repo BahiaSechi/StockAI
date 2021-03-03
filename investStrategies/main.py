c30f67a798626954479d570c7b7d10a401b516d7
Clément <cmservat@gmail.com>
2021-03-03 08:57:42 +0100

    new var env : starting funds and preferred ticker

--- investStrategies/main.py
+++ investStrategies/main.py
@@ -6,9 +6,11 @@ STOCKAI_PATH = os.environ['STOCKAI_PATH']
 STOCKAI_HOSTNAME = os.environ['STOCKAI_HOST']
 STOCKAI_PORT = os.environ['STOCKAI_PORT']
 STOCKAI_STRATEGY = os.environ['STOCKAI_STRATEGY']
+STOCKAI_TICKER = os.environ['STOCKAI_TICKER']
+STOCKAI_FUNDS = os.environ['STOCKAI_FUNDS']
 
 #env variables verification
-environ_list = ['STOCKAI_PATH', 'STOCKAI_HOSTNAME', 'STOCKAI_PORT', 'STOCKAI_STRATEGY']
+environ_list = ['STOCKAI_PATH', 'STOCKAI_HOSTNAME', 'STOCKAI_PORT', 'STOCKAI_STRATEGY', 'STOCKAI_TICKER', 'STOCKAI_FUNDS']
 for var in environ_list:
     if var in os.environ:
         print(f'{var} value is {os.environ[var]}')
