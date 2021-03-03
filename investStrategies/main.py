679bd1b7d5ed12bb06ade0327c34041e2dd8c1ce
Clément <cmservat@gmail.com>
2021-03-03 09:51:53 +0100

    remaniement des variables d'environnement

--- investStrategies/main.py
+++ investStrategies/main.py
@@ -1,21 +1,57 @@
-from investStrategies.MultipleTechInd.Investor import Investor
+import sys
+sys.path.append("/Users/clem/ENSICAEN/2A/stockai/")
 import os
+from investStrategies.MultipleTechInd.Investor import Investor
+
 
 #env variables definition
-STOCKAI_PATH = os.environ['STOCKAI_PATH']
-STOCKAI_HOSTNAME = os.environ['STOCKAI_HOST']
-STOCKAI_PORT = os.environ['STOCKAI_PORT']
-STOCKAI_STRATEGY = os.environ['STOCKAI_STRATEGY']
-STOCKAI_TICKER = os.environ['STOCKAI_TICKER']
-STOCKAI_FUNDS = os.environ['STOCKAI_FUNDS']
+environ_var = {
+    "default_path" : os.path.dirname(os.path.abspath(__file__)) + '/../',
+    "default_hostname" : '51.210.180.105',
+    "default_port" : 8081, 
+    "default_strategy" : 'RSISMA', 
+    "default_ticker" : 'AAPL', 
+    "default_funds" : 5000,
+    "default_goal" : 10000
+    }
 
-#env variables verification
-environ_list = ['STOCKAI_PATH', 'STOCKAI_HOSTNAME', 'STOCKAI_PORT', 'STOCKAI_STRATEGY', 'STOCKAI_TICKER', 'STOCKAI_FUNDS']
-for var in environ_list:
-    if var in os.environ:
-        print(f'{var} value is {os.environ[var]}')
-    else:
-        print(f'{var} does not exist')
+#project path
+try:
+    STOCKAI_PATH = os.environ['STOCKAI_PATH']
+except KeyError:
+    STOCKAI_PATH = environ_var["default_path"]
+#hostname
+try:
+    STOCKAI_HOSTNAME = os.environ['STOCKAI_HOST']
+except KeyError:
+    STOCKAI_HOSTNAME = environ_var["default_hostname"]
+#port
+try:
+    STOCKAI_PORT = os.environ['STOCKAI_PORT']
+except KeyError:
+    STOCKAI_PORT = environ_var["default_port"]
+#strategy
+try:
+    STOCKAI_STRATEGY = os.environ['STOCKAI_STRATEGY']
+except KeyError:
+    STOCKAI_STRATEGY = environ_var["default_strategy"]
+#ticker
+try:
+    STOCKAI_TICKER = os.environ['STOCKAI_TICKER']
+except KeyError:
+    STOCKAI_TICKER = environ_var["default_ticker"]
+#funds
+try:
+    STOCKAI_FUNDS = os.environ['STOCKAI_FUNDS']
+except KeyError:
+    STOCKAI_FUNDS = environ_var["default_funds"]
+#goal
+try:
+    STOCKAI_GOAL = os.environ['STOCKAI_GOAL']
+except KeyError:
+    STOCKAI_GOAL = environ_var["default_goal"]
+
+
 
 #starting investor
 investor = Investor(1000, 40, 'AAPL', 20)
