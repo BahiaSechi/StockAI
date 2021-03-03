afecebb623620abaa45b2bd5589e2cbe4ea0828d
bsechi <mdmushrm@gmail.com>
2021-03-03 09:12:03 +0100

    few modif and new component for strategies graphs

---
+++ supervision/src/app/component/chart-strat/chart-strat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-chart-strat',
+  templateUrl: './chart-strat.component.html',
+  styleUrls: ['./chart-strat.component.css']
+})
+export class ChartStratComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
